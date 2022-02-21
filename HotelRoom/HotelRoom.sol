dc804582def753e855f0967d17af124888f5d43f
SamanKeighobadi <saman.keighobadi@gmail.com>
2022-02-21 14:02:45 -0800

    hotel room contract

---
+++ HotelRoom/HotelRoom.sol
@@ -0,0 +1,35 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity 0.6.0;
+
+contract HotelRoom {
+    address payable public owner;
+
+    enum Status {
+        Vacant,
+        Occupied
+    }
+    Status currentStatus;
+    event Occupy(address _occupant, uint256 _value);
+
+    constructor() public {
+        owner = msg.sender;
+        currentStatus = Status.Vacant;
+    }
+
+    modifier OnlyWhileVanact() {
+        require(currentStatus == Status.Vacant, "Book is not vacant");
+        _;
+    }
+
+    modifier costs(uint256 _amount) {
+        require(msg.value >= _amount, "Not enough Ether provided");
+        _;
+    }
+
+    function room() public payable OnlyWhileVanact costs(2) {
+        currentStatus = Status.Occupied;
+        owner.transfer(msg.value);
+        emit Occupy(msg.sender, msg.value);
+    }
+}
