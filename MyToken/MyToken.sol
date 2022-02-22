eaf12a51b6ee419091b10469c0692954c994a39c
SamanKeighobadi <saman.keighobadi@gmail.com>
2022-02-22 13:58:40 -0800

    declerate variables and events

--- MyToken/MyToken.sol
+++ MyToken/MyToken.sol
@@ -2,8 +2,14 @@
 
 pragma solidity >=0.7.0 <0.9.0;
 
-contract MyToken{
+contract MyToken {
+    string public name;
+    string public symbol;
+    uint256 public priceToken;
+    uint256 public totalSupply;
+    uint8 public decimals;
+    address public owner;
+    mapping(address => uint256) public balances;
 
-
-
+    event Transfer(address from, address to, uint256 amount);
 }
